f6e6f01e9b5e0e6ad1fc57b8285da90e68c12aa0
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-02-15 07:15:08 -0700

    options

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log('Hola mundo');
+
+// USA EL SNIPPET CLG PARA HACER UN CONSOLE LOG, RAPIDO.
+// console.log('probando')
+// console.warn('este es un warning')
+// console.error('esto es un error')
+
+//si quieres imprimir en consola el nombre de la varible y su contenido puedes hacer
+let nombre = 'Maria'
+console.log({nombre}); //imprime "Nombre:
+
+let a = 20,
+    b = 30,
+    c = 'Maria',
+    d = 'eres linda';
+
+    // imprimes las variables dentro de una tabla y se guarda en un objeto temporal
+    console.table( { a, b, c, d } );
