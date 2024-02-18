1091732ab0a9fed4f3e78d583c069452cefbbb33
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-02-18 11:21:35 -0700

    Playing with console

---
+++ assets/js/alerts.js
@@ -0,0 +1,18 @@
+
+//ALERT
+// alert('Hola Mundo');
+
+// let nombre = prompt('Cual es tu nombre?', 'sin nombre');
+
+//PROMPT
+//jugando con las variables y lo que imprime en consola
+// si doy click en cancell aparecera null dentro de los asteriscos
+// si doy click en ok sin haber entrado nada apareceran solo los asteriscos.
+// let nombre = prompt('Cual es tu nombre?');
+// console.log('****' + nombre + '****');
+
+//CONFIRM
+// si damos click en ok, aparecera un true en consola
+// si doy click en ok, aparecerá false
+let seleccion = confirm('Estas seguro de hacer esta accion?');
+console.log(seleccion);
