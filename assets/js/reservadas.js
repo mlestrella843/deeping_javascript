edbac160ffd971bf21885eb069f2b53d48346430
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-02-18 13:28:27 -0700

    reservadas

---
+++ assets/js/reservadas.js
@@ -0,0 +1,17 @@
+
+// Aun que el uso de await como variable no de error, realmente al validarlo en una pagina para eso, dice que no se permite.
+// let await = "maria";
+// console.log(await);
+
+// Var la pagina de palabras reservadas y que no se pueden utilizar para otros fines.
+
+// es muy usado empezar la variable con _ y con $.
+
+let _numeros = 123; // se puede usar underscore antes de la variable
+
+let numerosPrimos$ = [1, 2, 3, 4, 5, 6, 7]; // uso de camel Case. Ademas de puede usar el simbolo de dolar.
+
+class JuegoAntonio {    // uso de mayuscula en el nombre de una clase.
+
+
+}
