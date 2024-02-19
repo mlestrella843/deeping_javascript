1da9898dd1d3ac90d7b85ff4515a6a329d78c7b0
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-02-18 17:20:42 -0700

    trucos en arreglos

---
+++ assets/js/arreglos2.js
@@ -0,0 +1,8 @@
+
+let juegos = [ ' Zelda', 'Mario', 'Metroid', 'Chrono' ];
+console.log( 'Largo: ', juegos.length ); // Para saber el largo de un arreglo
+
+let primero = juegos[0];
+let ultimo = juegos[ juegos.length - 1];
+
+console.log( { primero, ultimo } );
