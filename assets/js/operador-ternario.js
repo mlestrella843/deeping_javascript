b83e1cebab329d706610067a9eb84a271eba91c6
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-02-27 04:08:27 -0700

    solucion simplificada con operador ternario

--- assets/js/operador-ternario.js
+++ assets/js/operador-ternario.js
@@ -2,7 +2,7 @@
 // Funcion para saber si mi tienda esta abierta o no, mediante mi pagina web.
 
 const dia = 3;
-const horaActual = 13;
+const horaActual = 8;
 
 let horaApertura;
 let mensaje;
@@ -19,11 +19,15 @@ let mensaje;
 //Uso de OPERADOR TERNARIO
 // Aqui esta pregunta si el dias es 0 o es 6. es decir si es fin de semana?
 horaApertura = ( [0,6].includes(dia) ) ? 9 : 11; 
-if( horaActual >= horaApertura){
-    mensaje = 'La tienda esta abierta';
-}else{
-    mensaje = `La tienda esta cerrada, hoy abrimos a las ${horaApertura}`;
-}
+// Esta funcion clasica convertida a ternario 
+// ( horaActual >= horaApertura ) ? mensaje = 'La tienda esta abierta' : mensaje = `La tienda esta cerrada, hoy abrimos a las ${horaApertura}` ;
 
+// otra manera de hacerlo, mas simplificada
+mensaje = ( horaActual >= horaApertura ) ? 'La tienda esta abierta' : `La tienda esta cerrada, hoy abrimos a las ${horaApertura}`;
+// if( horaActual >= horaApertura){
+//     mensaje = 'La tienda esta abierta';
+// }else{
+//     mensaje = `La tienda esta cerrada, hoy abrimos a las ${horaApertura}`;
+// }
 
 console.log({ horaApertura, mensaje });
