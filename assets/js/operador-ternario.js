424816961945b937a80689798912538b629130f5
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-02-27 03:59:10 -0700

    Operador ternario

--- assets/js/operador-ternario.js
+++ assets/js/operador-ternario.js
@@ -8,21 +8,22 @@ let horaApertura;
 let mensaje;
 
 // if ( dia === 0  || dia === 6 ) {
-    if( [0,6].includes(dia)) {
-    console.log(' Es fin de semana ');
-    horaApertura = 9;
-}else{
-    console.log('Es dia de semana');
-    horaApertura=11;
-}
+//     if( [0,6].includes(dia)) {
+//     console.log(' Es fin de semana ');
+//     horaApertura = 9;
+// }else{
+//     console.log('Es dia de semana');
+//     horaApertura=11;
+// }
 
+//Uso de OPERADOR TERNARIO
+// Aqui esta pregunta si el dias es 0 o es 6. es decir si es fin de semana?
+horaApertura = ( [0,6].includes(dia) ) ? 9 : 11; 
 if( horaActual >= horaApertura){
     mensaje = 'La tienda esta abierta';
 }else{
     mensaje = `La tienda esta cerrada, hoy abrimos a las ${horaApertura}`;
 }
-
-
 
 
 console.log({ horaApertura, mensaje });
