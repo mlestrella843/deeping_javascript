4583d5dc5bacf2f56d0d86998d29ee2e2e099ffc
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-02-29 07:33:58 -0700

    Protips ternario

---
+++ assets/js/proTip-ternario.js
@@ -0,0 +1,12 @@
+
+const elMayor = ( a,b ) => {
+    return ( a > b ) ? a : b;
+};
+
+// Simplificanod la funciom
+const elMayor2 = ( a,b ) => ( a > b ) ? a : b;
+
+
+//Impresion en consola
+console.log(elMayor2( 20, 15 ));
+
