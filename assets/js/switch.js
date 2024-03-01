9e0c51553af93e384398fe39dda49ff9c3448864
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-03-01 05:45:42 -0700

    Switch

---
+++ assets/js/switch.js
@@ -0,0 +1,18 @@
+
+// En elk Switch la evaluacion corresponde a un triple igual ====
+// que evalua el valor que tiene y el tipo de dato.
+const dia = 2;
+
+switch( dia ) {
+    case 0:
+        console.log('Domingo');
+        break;
+    case 1:
+        console.log('Lunes');
+        break;
+    case 2:
+        console.log('Martes');
+        break;
+    default:
+        console.log(' No es Domingo, Lunes, ni Martes');
+}
