a51a08a7d5e2901486eb1ac91a265e57bd30dceb
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-03-02 07:45:56 -0700

    Do_While

--- assets/js/ciclos/while-do-while.js
+++ assets/js/ciclos/while-do-while.js
@@ -1,5 +1,6 @@
 
 //Ciclo While
+console.warn('While');
 const carros = ['Ford', 'Mazda', 'Honda', 'Toyota'];
 let i = 0;
 
@@ -8,5 +9,13 @@ while( i < carros.length ){
     i += 1;
 }
 
-//Ciclo Do-While
+// Ciclo Do-While
+// DO, la unica diferencia es que se ejecutara el bloque interno aunque sea una vez, y luego
+// ejecuta la condicion while.
+console.warn('Do-While');
+let j=3;
+do {
+    console.log( carros[ j ] );
+    j+=1;
+} while( j < carros.length );
 
