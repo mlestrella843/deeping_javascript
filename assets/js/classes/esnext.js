862afc445c0f18c28e15e620beb2f5f1aeee8a04
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2024-04-04 07:33:03 -0600

    Propiedades privadas en ES6

---
+++ assets/js/classes/esnext.js
@@ -0,0 +1,18 @@
+
+
+class Rectangulo {
+
+    area = 0;
+
+    constructor( base = 0, altura = 0) {
+        this.base = base;
+        this.altura = altura;
+
+        this.area = this.base * this.altura;
+    }
+
+}
+
+const rectangulo = new Rectangulo( 10, 15);
+
+console.log(rectangulo);
