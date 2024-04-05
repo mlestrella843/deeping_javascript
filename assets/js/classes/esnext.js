77cd73b110d7e1f251d072e14c28bcd4fc7bc215
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2024-04-05 07:25:24 -0600

    Uso de Propiedades y metodos privados en ES6 Javascript

--- assets/js/classes/esnext.js
+++ assets/js/classes/esnext.js
@@ -2,17 +2,24 @@
 
 class Rectangulo {
 
-    area = 0;
+    // Las propiedades privadas evitan que estas variables puedan modificarse.
+    #area = 0;
 
     constructor( base = 0, altura = 0) {
         this.base = base;
         this.altura = altura;
 
-        this.area = this.base * this.altura;
+        this.#area = this.base * this.altura;
+    }
+
+    //Aun los metodos privados no existen en ES6 y no estan disponibles usando el numeral
+    duplicarArea() {
+        console.log(` 'duplicar area' --> ${this.#area * 2 }`);
     }
 
 }
 
 const rectangulo = new Rectangulo( 10, 15);
-
+rectangulo.duplicarArea();
 console.log(rectangulo);
+
